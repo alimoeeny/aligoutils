bc41848e127c637f6c8ff24a367666b235ada9a8
Ali Moeeny <alimoeeny@gmail.com>
2022-09-08 14:11:22 -0400

    feat[spinner]:

---
+++ spinner.go
@@ -0,0 +1,10 @@
+package aliUtils
+
+var spinniThings = []string{"|", "/", "-", "\\"}
+
+var spinerI = 0
+
+func Spinner() string {
+	spinerI += 1
+	return spinniThings[spinerI%len(spinniThings)]
+}

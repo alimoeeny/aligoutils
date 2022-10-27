c683487a4291f039277c72597f38c14effd5c193
Ali Moeeny <alimoeeny@gmail.com>
2022-10-27 13:16:33 -0400

    exists ondisk

---
+++ fs_utils.go
@@ -0,0 +1,14 @@
+package aliU
+
+import "os"
+
+func ExistsOnDisk(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}

382230bd9e7ed3e49273d3873e5b240cfaa53a3a
Ali Moeeny <alimoeeny@gmail.com>
2024-07-02 11:25:48 -0400

    safe marshal and unmarshal

--- compress.go
+++ compress.go
@@ -2,9 +2,13 @@ package aliU
 
 import (
 	"bytes"
+	"compress/gzip"
 	"compress/zlib"
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"reflect"
 )
 
 func Zipb64(s string) string {
@@ -32,4 +36,36 @@ func Unzipb64(s string) (string, error) {
 	}
 	uzBytes, err := ioutil.ReadAll(zr)
 	return string(uzBytes), err
+}
+
+func SafeMarshall(s interface{}) (string, error) {
+	jsonData, err := json.Marshal(s)
+	if err != nil {
+		return "", err
+	}
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	gz.Write(jsonData)
+	gz.Close()
+	encoded := base64.StdEncoding.EncodeToString(b.Bytes())
+	return encoded, nil
+}
+
+func SafeUnmarshal(encoded string, out interface{}) error {
+	// Ensure out is a pointer
+	if reflect.ValueOf(out).Kind() != reflect.Ptr {
+		return fmt.Errorf("out parameter must be a pointer")
+	}
+	compressedData, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		return err
+	}
+	r, err := gzip.NewReader(bytes.NewReader(compressedData))
+	if err != nil {
+		return err
+	}
+	decompressedData, err := ioutil.ReadAll(r)
+
+	err = json.Unmarshal(decompressedData, &out)
+	return err
 }
---
+++ compress_test.go
@@ -0,0 +1,51 @@
+package aliU
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+)
+
+func Test_SaveMarshalUnmarshal(t *testing.T) {
+	RegisterTestingT(t)
+
+	{
+		ogStr := "ALIIIIIII"
+
+		safeM, err := SafeMarshall(ogStr)
+		Expect(err).To(BeNil())
+
+		var back string
+		err = SafeUnmarshal(safeM, &back)
+		Expect(err).To(BeNil())
+		Expect(ogStr).To(Equal(back))
+	}
+
+	{
+
+		type someType struct {
+			ID     string
+			Title  string
+			TS     int64
+			Length float64
+		}
+
+		ts := time.Now().UnixNano()
+		ogStr := someType{
+			"1234",
+			"ABCD",
+			ts,
+			3.141592,
+		}
+
+		safeM, err := SafeMarshall(ogStr)
+		Expect(err).To(BeNil())
+
+		var back someType
+		err = SafeUnmarshal(safeM, &back)
+		Expect(err).To(BeNil())
+		Expect(ogStr).To(Equal(back))
+	}
+
+}
